e6d0ca6e1d5072a5f88cb6d6186d86b0e68bcbc9
malasik <kenvilar@gmail.com>
2018-08-24 09:06:43 +0800

    Return Promise

--- handler.js
+++ handler.js
@@ -1,4 +1,9 @@
-module.exports.run = (event, context, callback) => {
-  console.log("This is a debug statement");
-  callback(null, "This is a test only");
+//module.exports.run = (event, context, callback) => {
+  //console.log("This is a debug statement");
+  //callback(null, "This is a test only");
+//}
+
+//instead of using callback from the top, we can also return Promises as well
+module.exports.run = (event) => {
+  return Promise.resolve("This is a test only");
 }
