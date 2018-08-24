e8b749b3a50f6bfebb79fe7f55262bc02e270f2a
malasik <kenvilar@gmail.com>
2018-08-24 09:12:00 +0800

    node 8 support async wait

--- handler.js
+++ handler.js
@@ -4,6 +4,11 @@
 //}
 
 //instead of using callback from the top, we can also return Promises as well
-module.exports.run = (event) => {
-  return Promise.resolve("This is a test only");
+// module.exports.run = (event) => {
+//   return Promise.resolve("This is a test only");
+// }
+
+//Since node 8 support async wait
+module.exports.run = async (event) => {
+  return "This is a test only";
 }
